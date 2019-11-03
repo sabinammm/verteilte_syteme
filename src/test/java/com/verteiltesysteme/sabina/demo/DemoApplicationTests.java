b0af7e3986167c79bf13264394054a628da0e6dc
Sabina Mollenhauer <s0563968@htw-berlin.de>
2019-11-03 21:37:33 +0100

    initial commit

---
+++ src/test/java/com/verteiltesysteme/sabina/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.verteiltesysteme.sabina.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
